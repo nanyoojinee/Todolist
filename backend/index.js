a25a8f319a65003ecf5d79cea67dbbba0b4c23d5
[AI7/2반] 이은석 <zhes4593@gmail.com>
2023-07-07 03:15:56 +0900

    :sparkles: mysql 연동

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,22 @@
 import 'dotenv/config'
 import { app } from "./src/app.js";
+import mysql from "mysql2";
 
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("MySQL server에 연결되었습니다!")
+})
 
 const PORT = process.env.SERVER_PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`정상적으로 서버를 시작하였습니다.  http://localhost:${PORT}`);
 });
+
