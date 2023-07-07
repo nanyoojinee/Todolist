1c6b936704a1610a57c554d34ae2421112178c1f
[AI7/2반] 이은석 <zhes4593@gmail.com>
2023-07-07 15:16:22 +0900

    :zap: dbconfig 분리

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,17 @@
 import 'dotenv/config'
 import { app } from "./src/app.js";
 import mysql from "mysql2";
-
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-});
-
-connection.connect(function (err) {
-    if (err) throw err;
-    console.log("MySQL server에 연결되었습니다!")
-})
+import dbconfig from './config/dbconfig.js'
+const connection = mysql.createConnection(dbconfig);
 
 const PORT = process.env.SERVER_PORT || 5000;
 
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("MySQL server에 연결되었습니다!")
+})
+
 app.listen(PORT, () => {
-  console.log(`정상적으로 서버를 시작하였습니다.  http://localhost:${PORT}`);
+  console.log(`정상적으로 서버를 시작하였습니다. z http://localhost:${PORT}`);
 });
 
